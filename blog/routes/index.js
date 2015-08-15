2e80bb8840552cce564bc41f1e99934bbf650b16
Zhenghaotao <zhenghaotao@foxmail.com>
2015-08-15 10:39:50 +0800

    实现了登陆，注册，退出的功能

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -18,6 +18,7 @@ module.exports = function (app) {
             error: req.flash('error').toString()
         });
     });
+    app.get('/reg',checkNotLogin);
     app.get('/reg', function (req, res) {
         res.render('reg', {
             title: '注册',
@@ -67,9 +68,33 @@ module.exports = function (app) {
     });
 
     app.get('/login', function (req, res) {
-        res.render('login', {title: '登陆'});
+        res.render('login', {
+            title: '登陆',
+            user: req.session.user,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
+        });
     });
     app.post('/login', function (req, res) {
+        //生成密码的 md5 值
+        var md5 = crypto.createHash('md5'),
+            password = md5.update(req.body.password).digest('hex');
+        //检查用户是否存在
+        User.get(req.body.name, function (err, user) {
+            if(!user){
+                req.flash('error','用户不存在!');
+                return res.redirect('/login');//用户不存在则跳转到登陆页面
+            }
+            //检查密码是否一致
+            if(user.password != password){
+                req.flash('error','密码错误!');
+                return res.redirect('/login');//密码错误则跳转到登陆页
+            }
+            //用户名密码都匹配后，将用户信息存入 session
+            req.session.user = user;
+            req.flash('success','登陆成功!');
+            res.redirect('/');//登楼成功后跳转到主页
+        });
     });
     app.get('/post', function (req, res) {
         res.render('post', {title: '发表'});
@@ -77,7 +102,25 @@ module.exports = function (app) {
     app.post('/post', function (req, res) {
     });
     app.get('/logout', function (req, res) {
+            req.session.user = null;
+            req.flash('success','登出成功!');
+            res.redirect('/');//登出成功后跳转到主页
     });
 
+
+    function checkLogin(req,res,next){
+        if(!req.session.suer){
+            req.flash('error','未登陆');
+            res.redirect('/login');
+        }
+        next();
+    }
+    function checkNotLogin(req,res,next){
+        if(req.session.user){
+            req.flash('error','已登陆!');
+            res.redirect('back');//返回之前的页面
+        }
+        next();
+    }
 
 };
