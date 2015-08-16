a0785cd6f2a9d649d75627eba6624ba63e0a13c4
Zhenghaotao <zhenghaotao@foxmail.com>
2015-08-16 12:27:22 +0800

    添加了标签功能

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -122,7 +122,8 @@ module.exports = function (app) {
     app.post('/post', checkLogin);
     app.post('/post', function (req, res) {
         var currentUser = req.session.user,
-            post = new Post(currentUser.name, req.body.title, req.body.post);
+            tags = [req.body.tag1, req.body.tag2, req.body.tag3],
+            post = new Post(currentUser.name, req.body.title, tags, req.body.post);
         post.save(function (err) {
             if (err) {
                 req.flash('error', err);
@@ -266,7 +267,37 @@ module.exports = function (app) {
             res.redirect('back');
         });
     });
+    app.get('/archive', function (req, res) {
+        Post.getArchive(function (err, posts) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            res.render('archive', {
+                title: '存档',
+                posts: posts,
+                user: req.session.user,
+                success: req.flash('success').toString(),
+                error: req.flash('error').toString()
+            });
+        });
+    });
 
+    app.get('/tags', function (req, res) {
+        Post.getTags(function (err, posts) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            res.render('tags', {
+                title: '标签',
+                posts: posts,
+                user: req.session.user,
+                success: req.flash('success').toString(),
+                error: req.flash('error').toString()
+            });
+        });
+    });
 
     function checkLogin(req, res, next) {
         if (!req.session.user) {
